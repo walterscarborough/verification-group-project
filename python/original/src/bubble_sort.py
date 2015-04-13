bce5f27864581df9fe0b2ff1bd3792b92d04b1d3
ResoluteFury <aaron.wood.gcca@gmail.com>
2015-04-13 15:28:26 -0500

    Fixed formatting for bubble_sort.py so it now only contains spaces (as opposed to tabs and spaces).

--- python/original/src/bubble_sort.py
+++ python/original/src/bubble_sort.py
@@ -5,7 +5,7 @@ def bubble_sort(array_in):
     # If array is None (null), return None (null)
     if array_in is None:
         return None
-		
+
     # If array size is 0 or 1, it is already sorted
     if len(array_in) < 2:
         return array_in
@@ -13,9 +13,9 @@ def bubble_sort(array_in):
     # Begin outer loop
     for i in range(len(array_in)):
         for j in range(0, len(array_in) - (i + 1)):
-            if array_in[j] > array_in[j+1]:
-                temp = array_in[j+1]
-                array_in[j+1] = array_in[j]
+            if array_in[j] > array_in[j + 1]:
+                temp = array_in[j + 1]
+                array_in[j + 1] = array_in[j]
                 array_in[j] = temp
 
     return array_in
